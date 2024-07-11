ab4e472ae04db895b9bc64cc4ac3008fc5af798b
blazereon <you@example.com>
2024-07-12 00:29:12 +0800

    its been so long

--- src/main/java/org/app/bantaysintadesktopapp/ReportController.java
+++ src/main/java/org/app/bantaysintadesktopapp/ReportController.java
@@ -1,6 +1,5 @@
 package org.app.bantaysintadesktopapp;
 
-import com.sun.javafx.charts.Legend;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -17,7 +15,6 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 
